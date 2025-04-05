43397906fff69a83216c29513d48ccf2c1412e4b
vadim-221212 <vdmed@bk.ru>
2025-04-06 01:29:19 +0300

    добавил корсы

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,10 @@
 import app from './app';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.PORT || 5006;
-
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
