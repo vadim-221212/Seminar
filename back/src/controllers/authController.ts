39f336b8ec891925a60b97831c35cdfaaf8422e8
vadim-221212 <vdmed@bk.ru>
2025-04-05 23:55:26 +0300

    back

--- back/src/controllers/authController.ts
+++ back/src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { createUser, findUserByEmail, getUserById } from '../models/user';
 
