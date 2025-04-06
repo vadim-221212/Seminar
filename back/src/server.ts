fddd727fcaba656c95ad63052c096a3f5a5b5b42
vadim-221212 <vdmed@bk.ru>
2025-04-07 01:48:42 +0300

    опять и опять b

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,11 @@
 import app from './app';
 import dotenv from 'dotenv';
+// @ts-ignore
 import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.PORT || 5006;
+// @ts-ignore
 app.use(cors());
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
